44f53450e2330c82e0e70aa79d67931810157d18
Thomas <thomast.harper@gmail.com>
2020-02-01 12:36:34 -0800

    Scene Transitions
    * Created a Teleporter Object and a Scene Controller to handle scene transitions.
    * Added GameObjectNotes to notate information about gameobjects.

---
+++ Assets/Scripts/Scene Management/SceneLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Singleton..
+    public static SceneLoader Instance;
+
+    // Set our Singleton Reference..
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadSceneWithString(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void LoadSceneWithBuildIndex(int buildIndexToLoad)
+    {
+        SceneManager.LoadScene(buildIndexToLoad);
+    }
+
+    public void MovePlayerAfterSceneLoad(GameObject player, Vector2 newPosition)
+    {
+        player.transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/Utility/Tools/GameObjectNotes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is a tool to be attached to any object in the project so we can get a clear understanding
+/// of the object without opening a bunch of scripts
+/// </summary>
+public class GameObjectNotes : MonoBehaviour
+{
+    [TextArea(1,10)]
+    public string Notes;
+}
