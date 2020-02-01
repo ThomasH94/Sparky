7eefed8b291c6ece52af625aae3fa2c5f9f93fd5
Thomas <thomast.harper@gmail.com>
2020-02-01 15:31:59 -0800

    Part Pickups
    * Added Robot Part and Part Variants for pickup

---
+++ Assets/Scripts/RobotPart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PartType
+{
+    Arm,
+    Leg,
+    Heart
+}
+
+/// <summary>
+/// This script will be the basis of the prefab for collecting Robot Parts
+/// </summary>
+public class RobotPart : MonoBehaviour
+{
+    public PartType partType;
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.gameObject.GetComponent<PlayerController>())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
