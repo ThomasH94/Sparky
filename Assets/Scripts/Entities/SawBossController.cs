6e0e20437832313e62885938e3c4285639203e87
Steven-PC\Steven <steve@phenomgames.co>
2020-02-02 14:58:48 -0800

    Progress on saw boss

---
+++ Assets/Scripts/Entities/SawBossController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBossController : EnemyBase
+{
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        if (detectedPlayer && isInAttackRange)
+        {
+            DoAbility(1);
+        }
+    }
+}
