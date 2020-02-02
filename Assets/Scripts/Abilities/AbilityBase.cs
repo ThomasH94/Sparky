acca0dfd3cfb423689c7d992e069b4d6d5f3b788
Steven-PC\Steven <steve@phenomgames.co>
2020-02-02 01:07:08 -0800

    Progress on player movement, animations, and abilities

---
+++ Assets/Scripts/Abilities/AbilityBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbilityBase : MonoBehaviour
+{
+    public float cooldown;
+
+    public float cooldownRemaining;
+
+    private IEnumerator cooldownTimerRoutine;
+
+    public virtual void DoUse()
+    {
+        if (cooldownRemaining > 0)
+            return;
+
+        cooldownTimerRoutine = CooldownTimer(cooldown);
+        StartCoroutine(cooldownTimerRoutine);
+    }
+
+    private IEnumerator CooldownTimer(float duration)
+    {
+        cooldownRemaining = cooldown;
+
+        while (cooldownRemaining > 0)
+        {
+            cooldownRemaining -= Time.deltaTime;
+            yield return null;
+        }
+
+        cooldownRemaining = 0;
+    }
+}
---
+++ Assets/Scripts/Abilities/Ability_Swipe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_Swipe : AbilityBase
+{
+    public override void DoUse()
+    {
+        if (cooldownRemaining > 0)
+            return;
+
+        base.DoUse();
+
+        //if()
+    }
+}
