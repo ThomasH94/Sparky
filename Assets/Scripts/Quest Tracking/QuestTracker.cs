f89e8155411fc0e312d3e5420c76fcacf64dc509
Thomas <thomast.harper@gmail.com>
2020-02-01 17:47:28 -0800

    Updating Quest Tracker
    * Still cleaning up the Quest Tracker

--- Assets/Scripts/Quest Tracking/QuestTracker.cs
+++ Assets/Scripts/Quest Tracking/QuestTracker.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// This script will track all of the relevant information for completing the game and determining an ending
@@ -12,6 +14,18 @@ public class QuestTracker : MonoBehaviour
 
     public int enemiesDefeated;
     public int bossesDefeated;
+
+    #region UI
+    public GameObject endGameScreen;
+    private Image endCard;
+    private TextMeshProUGUI endGameText;
+    public Sprite goodEndingSprite, badendingSprite, greyEndingSprite;
+    #endregion
+
+    private void Start()
+    {
+        endCard = endGameScreen.GetComponentInChildren<Image>();
+    }
 
     public void IncreasePartsCollected()
     {
@@ -26,5 +40,29 @@ public class QuestTracker : MonoBehaviour
     public void IncreaseBossesDefeated()
     {
 
+    }
+
+    public void CheckForEnding()
+    {
+        if(enemiesDefeated <= 0)
+        {
+            if(bossesDefeated <= 0)
+            {
+                // Good Ending
+                endCard.sprite = goodEndingSprite;
+            }
+        }
+        else if(enemiesDefeated > 1 && enemiesDefeated < 10 || bossesDefeated > 1 && bossesDefeated < 3)
+        {
+            // Grey Ending
+            endCard.sprite = greyEndingSprite;
+        }
+        else
+        {
+            // Bad Ending
+            endCard.sprite = badendingSprite;
+        }
+
+        endGameScreen.SetActive(true);
     }
 }
