4ccab9de9c97b2300fe2bdf1f061a4afab6c7819
Steven-PC\Steven <steve@phenomgames.co>
2020-02-01 23:05:32 -0800

    Made player controller use 3D model and physics

--- Assets/Scripts/Entities/EntityController.cs
+++ Assets/Scripts/Entities/EntityController.cs
@@ -3,48 +3,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum FacingDirection
-{
-    Down, Left, Up, Right
-}
+//public enum FacingDirection
+//{
+//    Down, Left, Up, Right
+//}
 
 public class EntityController : Damageable
 {
+    //public Action<FacingDirection> onFacingDirectionUpdated;
+
     public float moveSpeed = 10f;
-    public FacingDirection facingDirection;
 
     [SerializeField]
-    protected Rigidbody2D body = null;
+    private float rotSpeed = 2f;
+    //public FacingDirection facingDirection;
+
+    [SerializeField]
+    protected Rigidbody body = null;
+    [SerializeField]
+    protected float attackRange = 2f;
+
+    protected Transform lookAt;
 
     protected virtual void Reset()
     {
-        body = GetComponent<Rigidbody2D>();
+        body = GetComponent<Rigidbody>();
+    }
+
+    protected virtual void Start()
+    {
+        lookAt = new GameObject(gameObject.name + "_LookAt").transform;
+        lookAt.gameObject.hideFlags = HideFlags.HideInHierarchy;
     }
 
     protected virtual void FixedUpdate()
     {
-        
+
     }
 
-    protected virtual void Move(Vector2 directionalInput)
+    protected virtual void Move(Vector3 directionalInput)
     {
         if (Math.Abs(directionalInput.x) > .01f || Math.Abs(directionalInput.y) > .01f)
         {
-            body.MovePosition(body.position + directionalInput.normalized * moveSpeed * Time.deltaTime);
+            //if (body.velocity.magnitude < moveSpeed)
+            //    body.AddForce(directionalInput.normalized * moveSpeed * 100f * Time.deltaTime);
+            Vector3 newDir = new Vector3(directionalInput.normalized.x, 0, directionalInput.normalized.y);
+            body.MovePosition(body.position + newDir * moveSpeed * Time.deltaTime);
 
-            SetFacingDirection(directionalInput);
+            lookAt.position = body.position;
+            lookAt.LookAt(body.position + newDir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookAt.rotation, rotSpeed);
+
+            //body.SetRotation(Mathf.Lerp(body.rotation, Vector3.SignedAngle(Vector3.up, directionalInput, Vector3.forward), rotSpeed));
+            //transform.forward = newDir;
+            //Vector2.Lerp(transform.up, directionalInput, rotSpeed);
+
+            //SetFacingDirection(directionalInput);
         }
+        //else
+        //    body.velocity = Vector3.zero;
     }
 
-    protected virtual void SetFacingDirection(Vector2 dir)
-    {
-        if (dir.y > .01f)
-            facingDirection = FacingDirection.Up;
-        else if (dir.y < -.01f)
-            facingDirection = FacingDirection.Down;
-        else if (dir.x > .01f)
-            facingDirection = FacingDirection.Right;
-        else if (dir.x < -.01f)
-            facingDirection = FacingDirection.Left;
-    }
+    //protected virtual void SetFacingDirection(Vector2 dir)
+    //{
+    //    if (dir.y > .01f)
+    //        facingDirection = FacingDirection.Up;
+    //    else if (dir.y < -.01f)
+    //        facingDirection = FacingDirection.Down;
+    //    else if (dir.x > .01f)
+    //        facingDirection = FacingDirection.Right;
+    //    else if (dir.x < -.01f)
+    //        facingDirection = FacingDirection.Left;
+
+    //    onFacingDirectionUpdated?.Invoke(facingDirection);
+    //}
 }
---
+++ Assets/Scripts/Entities/EntityGraphics.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityGraphics : MonoBehaviour
+{
+    [SerializeField]
+    protected EntityController controller;
+
+    protected virtual void Start()
+    {
+        //controller.onFacingDirectionUpdated += UpdateFacingDirection;
+    }
+
+    //protected virtual void UpdateFacingDirection(FacingDirection newDir) { }
+}
